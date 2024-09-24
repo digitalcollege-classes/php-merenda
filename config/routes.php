30e16835b4f5e28283aafa53aa397bd7afc77cc7
jeas-github <jeas@outlook.com>
2024-09-24 20:36:57 -0300

    feat: Cria página de listar usuários

--- config/routes.php
+++ config/routes.php
@@ -14,17 +14,20 @@ return [
     '/categorias/adicionar' => [CategoryController::class, 'add'],
     '/categorias/editar' => [CategoryController::class, 'edit'],
     '/categorias/remover' => [CategoryController::class, 'remove'],
-   
+
     '/clientes/listar' => [CustomerController::class, 'list'],
     '/clientes/adicionar' => [CustomerController::class, 'add'],
     '/clientes/editar' => [CustomerController::class, 'edit'],
     '/clientes/remover' => [CustomerController::class, 'remove'],
-   
+
     '/produtos/listar' => [ProductController::class, 'list'],
     '/produtos/adicionar' => [ProductController::class, 'add'],
     '/produtos/editar' => [ProductController::class, 'edit'],
     '/produtos/remover' => [ProductController::class, 'remove'],
-    
-    '/usuarios/adicionar' => [UserController::class, 'add'],
-];
 
+    '/usuarios/listar' => [UserController::class, 'list'],
+    '/usuarios/adicionar' => [UserController::class, 'add'],
+    '/usuarios/editar' => [UserController::class, 'edit'],
+    '/usuarios/remover' => [UserController::class, 'remove'],
+
+];
--- views/user/edit.php
+++ views/user/edit.php
@@ -2,7 +2,7 @@
     <div class="card my-4 mt-5">
         <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
             <div class="d-flex justify-content-between bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
-                <h6 class="text-white text-capitalize ps-3 mt-1">Novo Usuário</h6>
+                <h6 class="text-white text-capitalize ps-3 mt-1">Editar Usuário</h6>
 
                 <a href="/usuarios/listar" class="btn btn-light mb-1 me-4">
                     Listar
@@ -19,20 +19,13 @@
                 </div>
 
                 <div class="input-group input-group-outline mb-3">
-                    <label class="w-30" for="email">
-                        Email
-                        <input type="text" name="email" id="email" class="form-control">
+                    <label class="w-30" for="endereco">
+                        Endereço
+                        <input type="text" name="endereco" id="endereco" class="form-control">
                     </label>
                 </div>
 
-                <div class="input-group input-group-outline mb-3">
-                    <label class="w-30" for="password">
-                        Senha
-                        <input type="password" name="password" id="password" class="form-control">
-                    </label>
-                </div>
-
-                <button class="btn btn-primary btn-lg w-30">Cadastrar</button>
+                <button class="btn btn-primary btn-lg w-30">Pronto</button>
             </form>
         </div>
     </div>
