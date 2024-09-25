b00f04f525397cefd2b72b8193e04774c1788548
Tiago Lima <47084105+tiagolimar@users.noreply.github.com>
2024-09-25 19:27:35 +0000

    atualizacao de rotas

--- config/routes.php
+++ config/routes.php
@@ -4,13 +4,9 @@ declare(strict_types=1);
 
 use App\Controller\CategoryController;
 use App\Controller\CustomerController;
-<<<<<<< HEAD
 use App\Controller\ProductController;
 use App\Controller\UserController;
-=======
->>>>>>> da98fe3 (ajuste na rota)
 use App\Controller\OrderController;
-use App\Controller\ProductController;
 
 return [
     '/' => [CategoryController::class, 'list'],
