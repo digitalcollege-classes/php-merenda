970a4d4838abff05ee1baa4a3aa7124a56ed6421
Mathew Vieira <mvieirabezerra@gmail.com>
2024-10-08 17:42:08 -0300

    updates AbstractController render logic

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -4,22 +4,11 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-class AuthController
+class AuthController extends AbstractController
 {
-    protected function render(string $view): void
-    {
-        include '../views/_layouts/head.php';
-        
-        include '../views/_components/content.php';
-
-        include "../views/{$view}.php";
-
-        include '../views/_layouts/footer.php';
-    }
-
     public function login(): void
     {
-        $this->render('auth/login');
+        parent::render('auth/login', true, false);
     }
 
 }
