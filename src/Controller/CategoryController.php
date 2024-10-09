fd56d749d41f1fd9ec1c6502b445bb272a2d8ee3
alessandrofeitoza01@gmail.com <alessandrofeitoza01@gmail.com>
2024-10-08 21:56:29 -0300

    CRUD de categorias

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -4,16 +4,44 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Connection\Connection;
+use App\Entity\Category;
+use Doctrine\ORM\EntityManager;
+use Doctrine\Persistence\ObjectRepository;
+
 class CategoryController extends AbstractController implements ControllerInterface
 {
+    private EntityManager $entityManager;
+    private ObjectRepository $repository;
+
+    public function __construct()
+    {
+        $this->entityManager = Connection::getEntityManager();
+        $this->repository = $this->entityManager->getRepository(Category::class);
+    }
+
     public function add(): void
     {
-        $this->render('category/add');
+        if (true === empty($_POST)) {
+            $this->render('category/add');
+            return;
+        }
+
+        $object = new Category($_POST['name']);
+        $object->setDescription($_POST['description']);
+        $object->setImage($_POST['image']);
+
+        $this->entityManager->persist($object);
+        $this->entityManager->flush();
+
+        header('location: /categorias/listar');
     }
 
     public function list(): void
     {
-        $this->render('category/list');
+        $this->render('category/list', [
+            'categories' => $this->repository->findAll(),
+        ]);
     }
 
     public function edit(): void
@@ -23,6 +51,11 @@ class CategoryController extends AbstractController implements ControllerInterfa
 
     public function remove(): void
     {
-        echo "excluir...";
+        $category = $this->repository->find($_GET['id']);
+
+        $this->entityManager->remove($category);
+        $this->entityManager->flush();
+
+        header('location: /categorias/listar');
     }
 }
